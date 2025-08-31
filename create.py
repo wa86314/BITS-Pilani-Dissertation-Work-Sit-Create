ce0bc2825e8bce56682d0befc49dec09b9059cf4
vboxuser <vboxuser@DevOpsLab.myguest.virtualbox.org>
2025-08-31 12:22:18 +0530

    major bug fix

---
+++ create.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+
+from connection import db_conn
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "INFO: This Microservice is used for adding record to the Database"
+
+@app.route('/create', methods=['POST'])
+def create():
+
+    data = request.get_json()
+    stud_name = data.get('name')
+    db_cursor = db_conn.cursor()
+    query = "INSERT INTO student (name) VALUES (%s)"
+    value = {stud_name} 
+    db_cursor.execute(query, value)
+    db_conn.commit()
+
+    return f"Record added for {stud_name} successfully!"
+  
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+db_conn.close()
+
+
+
